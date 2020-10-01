c6c8c7359d3f0cce104df6227588cfb513197a7f
Arseni Pantsialei <arsenipantsialei@gmail.com>
2020-10-01 20:43:21 +0300

    feat: add three functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (array && array.length > 0) {
+        let currentArr = array[0];
+
+        for (let i = 1; i < array.length; i++) {
+          let checkArr = array[i];
+          if (checkArr < currentArr) {
+            currentArr = checkArr; 
+          }
+        }
+        return currentArr;
+      } else {
+        return 0;
+      }
+}   
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length > 0) {
+        let currentArr = array[0];
+
+        for (let i = 1; i < array.length; i++) {
+          let checkArr = array[i];
+          if (checkArr > currentArr) {
+            currentArr = checkArr; 
+          }
+        }
+        return currentArr;
+      } else {
+        return 0;
+      }
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+    let count = 0;
+    
+    if (array && array.length > 0) {
+        let arrLength = array.length;
+    
+        for (let i = 0; i < array.length; i++) {
+            count  += array[i];
+      
+          }
+        return count / arrLength;
+      } else {
+          return 0
+      }
 }
